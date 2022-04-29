95caac029d4bac46c17e11221e364f80f637e666
Denis Ognev <den.is.ognev@yandex.ru>
2022-04-29 22:47:47 +0300

    Added 3 mode of task for interactive interaction

--- todo_service/todo/static/todo/js/base.js
+++ todo_service/todo/static/todo/js/base.js
@@ -4,7 +4,7 @@ const uri_api = 'api/v1/todo';
 let taskGroups = [];
 
 function addTaskInnerHTML(item,tag='#tasks_list') {
-    document.querySelector(tag).innerHTML += buildTasks(item);
+    document.querySelector(tag).innerHTML += buildTaskPreviewMode(item);
 }
 
 function displayItems(data) {
@@ -20,13 +20,66 @@ function getGroups() {
         .catch(error => console.error('Unable to get items.', error));
 }
 
-function buildTasks(itemTask) {
+// function buildTasks(itemTask) {
+//     return `
+//     <div id=${itemTask.id}>
+//         <li>${itemTask.title}</li> 
+//     </div>
+//     `;
+// }
+
+function buildTaskEditMode(itemTask) {
     return `
-    <div>
-        <li id=${itemTask.id}>${itemTask.title}</li> 
+    <div id=${itemTask.id} class="edit__mode">
+        <div class="task_data"> 
+            <input type="text" value="${itemTask.title}">
+        </div>
+        <div class="task_buttons">
+            <div class="task__button__cancel" >
+                <button >X</button>
+            </div>
+            <div class="task__button__agree">
+                <button >OK</button>
+            </div>
+        </div>
+    </div> 
+    `;
+}
+
+
+function buildTaskPreviewMode(itemTask) {
+    return `
+    <div id=${itemTask.id} class="preview_mode">
+        <button>${itemTask.title}</button>
     </div>
     `;
 }
+
+function buildTaskAdvancedMode(itemTask) {
+    return `
+    <div id=${itemTask.id} class="advanced__mode">
+        <div class="task_data"> 
+            <span>${itemTask.title}</span>
+        </div>
+        <div class="task_buttons">
+            <div class="task__button__del" >
+                <button >DEL</button>
+            </div>
+            <div class="task__button__edit">
+                <button >EDIT</button>
+            </div>
+            <div class="task__button__done">
+                <button >OK</button>
+            </div>
+        </div>
+    </div> 
+    `;
+}
+
+
+
+
+
 
 
 
