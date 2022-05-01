548fc298a82ac9d1e09651eb5de22df409ead707
Denis Ognev <den.is.ognev@yandex.ru>
2022-05-01 22:23:10 +0300

    added comments in base.js

--- todo_service/todo/static/todo/js/base.js
+++ todo_service/todo/static/todo/js/base.js
@@ -1,5 +1,8 @@
 'use strict';
 
+/* This scripts contain auto-comments */
+
+/* Defining constants. */
 const uri_api = 'api/v1/todo';
 
 const FILTER_DIV_GROUP = ".todo__body__filter";
@@ -16,9 +19,17 @@ const CSS_CLASS_COMPLETED_TASK = 'completed_task';
 
 const TAG_TASKS = '#tasks_list';
 
+
 let tasksList = [];
 let FILTER_SELECTED = ID_FILTER_BUTTON_ACTIVE ; // DEFAULT
 
+/**
+ * If the filter is all, add the task to the list. If the filter is active and the task is not
+ * completed, add the task to the list. If the filter is completed and the task is completed, add the
+ * task to the list.
+ * @param itemTask - the object that contains the task data
+ * @param [filterIdSelected] - the id of the filter button that was clicked
+ */
 function addTaskInnerHTML(itemTask,filterIdSelected=ID_FILTER_BUTTON_ACTIVE) {
     if (filterIdSelected == ID_FILTER_BUTTON_ALL)
         document.querySelector(TAG_TASKS).innerHTML += buildTaskPreviewMode(itemTask);
@@ -28,16 +39,24 @@ function addTaskInnerHTML(itemTask,filterIdSelected=ID_FILTER_BUTTON_ACTIVE) {
         document.querySelector(TAG_TASKS).innerHTML += buildTaskPreviewMode(itemTask);
 }
 
+/**
+ * If the data array has a length, then for each item in the first element of the data array, add the
+ * task to the inner HTML of the tasks element.
+ * @param data - is an array of arrays, each array contains the tasks of a specific category.
+ * @param [filterIdSelected] - the id of the filter button that was clicked.
+ */
 function displayItems(data,filterIdSelected=ID_FILTER_BUTTON_ACTIVE) {
     document.querySelector(TAG_TASKS).innerHTML = "";
     tasksList = data;
-    // data[0].forEach((itemTask) => addTaskInnerHTML(itemTask,filterIdSelected));
     if(data.length)
-        // data[0].map( (itemTask) => () => addTaskInnerHTML(itemTask,filterIdSelected));
         data[0].forEach((itemTask) => addTaskInnerHTML(itemTask,filterIdSelected));
 }
 
 
+/**
+ * Get the data from the API, then display the items, then catch any errors.
+ * @param [filterIdSelected] - the id of the filter selected
+ */
 function getGroups(filterIdSelected=FILTER_SELECTED) {
     fetch(uri_api)
         .then(response => response.json())
@@ -45,6 +64,11 @@ function getGroups(filterIdSelected=FILTER_SELECTED) {
         .catch(error => console.error('Unable to get items.', error));
 }
 
+/**
+ * It takes an object as an argument and returns a string of HTML.
+ * @param itemTask - {
+ * @returns A string of HTML code.
+ */
 function buildTaskEditMode(itemTask) {
     return `
     <div id=${itemTask.id}>
@@ -66,6 +90,12 @@ function buildTaskEditMode(itemTask) {
 }
 
 
+/**
+ * This function takes in an object and returns a string of HTML that represents a task in preview
+ * mode.
+ * @param itemTask - {
+ * @returns A string of HTML code.
+ */
 function buildTaskPreviewMode(itemTask) {
     return `
     
@@ -76,13 +106,13 @@ function buildTaskPreviewMode(itemTask) {
     </div>
     `;
 }
-/* <button class="back_button__advanced__mode${itemTask.is_completed ? ' '+CSS_CLASS_COMPLETED_TASK : ''}" onclick="button_cancel_ADVANCEDMODE(${itemTask.id})">${itemTask.title}</button> */
-/* <span class="${itemTask.is_completed ? ' '+CSS_CLASS_COMPLETED_TASK : ''}" onclick="button_cancel_ADVANCEDMODE(${itemTask.id})">${itemTask.title}</span> */
-{/* <button class="back_button__advanced__mode${itemTask.is_completed ? ' '+CSS_CLASS_COMPLETED_TASK : ''}" 
-                    onclick="button_cancel_ADVANCEDMODE(${itemTask.id})">
-                    ${itemTask.title}
-                </button> */}
-// <span class="${itemTask.is_completed ? CSS_CLASS_COMPLETED_TASK : ''}">${itemTask.title}</span>
+
+
+/**
+ * It takes an object as an argument and returns a string of HTML.
+ * @param itemTask - {
+ * @returns A string of HTML code.
+ */
 function buildTaskAdvancedMode(itemTask) {
     return `
     <div id=${itemTask.id}>
@@ -111,6 +141,12 @@ function buildTaskAdvancedMode(itemTask) {
     `;
 }
 
+/**
+ * It returns a string that contains a div element with an id and a class, and a button element with an
+ * onclick event.
+ * @param filterId - the id of the filter button that was clicked
+ * @returns A string of HTML code.
+ */
 function buildButtonFilterAll(filterId) {
     return `
     <div id="${ID_FILTER_BUTTON_ALL}" class="${filterId == ID_FILTER_BUTTON_ALL ? CSS_CLASS_FILTER_SELECTED : ''}">
@@ -136,9 +172,10 @@ function buildButtonFilterCompleted(filterId) {
 }
 
 
-// ID_FILTER_BUTTON_ALL
-// ID_FILTER_BUTTON_ACTIVE
-// ID_FILTER_BUTTON_COMPLITED
+/**
+ * It builds the buttons for the filters, and then displays the items based on the filter selected.
+ * @param [filterIdSelected] - the id of the filter selected
+ */
 function buildButtonsFilters(filterIdSelected=FILTER_SELECTED){
     if(filterIdSelected.id != undefined) filterIdSelected = filterIdSelected.id;
     document.querySelector(FILTER_DIV_GROUP).innerHTML = "";
@@ -149,10 +186,14 @@ function buildButtonsFilters(filterIdSelected=FILTER_SELECTED){
     FILTER_SELECTED=filterIdSelected;
 }
 
+/**
+ * When the user clicks the Add button, the addTask function is called. The function gets the value of
+ * the text box and creates a new item object. If the title is not empty, the function uses the fetch
+ * API to call the API's add method. The function then calls the getGroups function to update the page
+ * with the new list.
+ */
 function addTask() {
     const newTaskTitle = document.getElementById('#newtask_title');
-
-
     const item = {
         id: 0,
         title: newTaskTitle.value.trim()
@@ -169,16 +210,20 @@ function addTask() {
         })
             .then(response => response.json())
             .then(() => getGroups())
-            // .then(data => item[0].id = data)
-            // .then(addTaskInnerHTML(item))
+            .then(() => newTaskTitle.value = "")
             .catch(error => console.error('Unable to add item.', error))
-           
     }
-    
-    newTaskTitle.value = "";
 }
 
 
+/**
+ * It returns an object with the task's id, title, and whether or not it's completed.
+ * @param id - the id of the task
+ * @returns An object with the following properties:
+ * id: the id of the task
+ * title: the title of the task
+ * is_completed: a boolean value indicating whether the task is completed or not
+ */
 function getTaskAttributes_PREVIEWMODE(id){
     const divTask = document.getElementById(id);
     if(Boolean(divTask.querySelector(CSS_CLASS_PREVIEW_MODE))){
@@ -191,6 +236,15 @@ function getTaskAttributes_PREVIEWMODE(id){
     } else return null;  
 }
 
+/**
+ * If the task is in edit mode, return an object containing the task's id, title, and whether or not
+ * it's completed. Otherwise, return null.
+ * @param id - the id of the task
+ * @returns an object with the following properties:
+ * id: the id of the task
+ * title: the title of the task
+ * is_completed: a boolean value indicating whether the task is completed or not
+ */
 function getTaskAttributes_EDITMODE(id){
     const divTask = document.getElementById(id+'');
     if(Boolean(divTask.querySelector(CSS_CLASS_EDIT_MODE))){
@@ -203,6 +257,14 @@ function getTaskAttributes_EDITMODE(id){
     } else return null;  
 }
 
+/**
+ * It returns an object with the task's id, title, and whether or not it's completed.
+ * @param id - the id of the task
+ * @returns An object with the following properties:
+ * id: The id of the task
+ * title: The title of the task
+ * is_completed: A boolean value indicating whether the task is completed or not
+ */
 
 function getTaskAttributes_ADVANCEDMODE(id){
     const divTask = document.getElementById(id+'');
@@ -217,6 +279,10 @@ function getTaskAttributes_ADVANCEDMODE(id){
 }
 
 
+/**
+ * If the divTask exists, then change the status of the task to completed.
+ * @param id - the id of the task
+ */
 function button_done_ADVANCEDMODE(id) {
     id = id+''
     const divTask = document.getElementById(id);
@@ -226,6 +292,10 @@ function button_done_ADVANCEDMODE(id) {
     } 
 }
 
+/**
+ * If the divTask exists, then change the status of the task to false.
+ * @param id - the id of the task
+ */
 function button_undone_ADVANCEDMODE(id) {
     id = id+''
     const divTask = document.getElementById(id);
@@ -235,6 +305,11 @@ function button_undone_ADVANCEDMODE(id) {
     } 
 }
 
+/**
+ * It takes the id of a task, gets the task's attributes, and then replaces the task's html with the
+ * html for the task in edit mode.
+ * @param id - the id of the task
+ */
 function button_edit_ADVANCEDMODE(id) {
     id = id+''
     let itemTask = {}
@@ -246,6 +321,12 @@ function button_edit_ADVANCEDMODE(id) {
     } 
 }
 
+/**
+ * The function button_cancel_ADVANCEDMODE() is called when the user clicks the Cancel button in the
+ * Advanced Mode. The function gets the task attributes from the task div, and then rebuilds the task
+ * div in Preview Mode.
+ * @param id - the id of the task
+ */
 function button_cancel_ADVANCEDMODE(id) {
     id = id+''
     let itemTask = {}
@@ -257,6 +338,10 @@ function button_cancel_ADVANCEDMODE(id) {
     } 
 }
 
+/**
+ * If the divTask exists, then get the task attributes and build the task preview mode.
+ * @param id - the id of the task
+ */
 function button_cancel_EDITMODE(id) {
     id = id+''
     let itemTask = {}
@@ -268,6 +353,12 @@ function button_cancel_EDITMODE(id) {
 }
 
 
+/**
+ * The function button_PREVIEWMODE() takes an id as an argument, and then uses that id to find the div
+ * with the same id, and then uses the function getTaskAttributes_PREVIEWMODE() to get the attributes
+ * of the div, and then uses the function buildTaskAdvancedMode() to build the div with the attributes.
+ * @param id - the id of the div that contains the task
+ */
 function button_PREVIEWMODE(id) {
     id = id+''
     let itemTask = {}
@@ -278,6 +369,11 @@ function button_PREVIEWMODE(id) {
     } 
 }
 
+/**
+ * When the button is clicked, the function will delete the task from the database and then remove the
+ * task from the DOM.
+ * @param id - the id of the task
+ */
 function button_delete_ADVANCEDMODE(id) {
     const divTask = document.getElementById(id);
     fetch(`${uri_api}/${id}`, {
@@ -296,6 +392,11 @@ function button_delete_ADVANCEDMODE(id) {
 
 
 
+/**
+ * It takes the id of the task, gets the attributes of the task, and then sends a PUT request to the
+ * API to update the task.
+ * @param id - the id of the task
+ */
 function button_accept_EDITMODE(id) {
     id=id+'';
     let itemTask = getTaskAttributes_EDITMODE(id);
@@ -315,6 +416,12 @@ function button_accept_EDITMODE(id) {
     }
 }
 
+/**
+ * It takes the id of a task and a boolean value (true or false) and updates the task's status to the
+ * boolean value.
+ * @param id - the id of the task
+ * @param [taskStatus=true] - true or false
+ */
 
 function task_completed_changestatus(id,taskStatus = true) {
     id=id+'';
@@ -335,4 +442,3 @@ function task_completed_changestatus(id,taskStatus = true) {
             .catch(error => console.error('Unable to add item.', error));
     }
 }
-
